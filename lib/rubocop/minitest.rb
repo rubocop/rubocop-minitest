09a812bef5451b9ddd6dc0099960a1f821b72e6e
Koichi ITO <koic.ito@gmail.com>
2019-08-02 15:33:32 +0900

    bundle gem rubocop-minitest

---
+++ lib/rubocop/minitest.rb
@@ -0,0 +1,8 @@
+require "rubocop/minitest/version"
+
+module Rubocop
+  module Minitest
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/minitest/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Minitest
+    VERSION = "0.1.0"
+  end
+end
