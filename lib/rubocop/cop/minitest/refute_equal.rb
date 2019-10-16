178d2f70a5b3b660dfbd11ff32bc5040755932a6
Herwin <herwinw@herwinw.nl>
2019-10-16 08:44:42 +0200

    Fix a typo in the documentation of refute_equal (#30)

--- lib/rubocop/cop/minitest/refute_equal.rb
+++ lib/rubocop/cop/minitest/refute_equal.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Cop
     module Minitest
       # This cop enforces the use of `refute_equal(expected, object)`
-      # over `assert_equal(expected != actual)` or `assert(! expected -= actual)`.
+      # over `assert_equal(expected != actual)` or `assert(! expected == actual)`.
       #
       # @example
       #   # bad
