eae3af7c547f8d8afb77e2ad181eebb95c5ad7e9
Tejas Bubane <tejasbubane@gmail.com>
2019-11-07 17:18:10 +0530

    [Fix #32] Add new cop assert_empty_literal
    Closes #32

---
+++ test/rubocop/cop/minitest/assert_empty_literal_test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AssertEmptyLiteralTest < Minitest::Test
+  def setup
+    @cop = RuboCop::Cop::Minitest::AssertEmptyLiteral.new
+  end
+
+  def test_registers_offense_when_asserting_empty_array
+    assert_offense(<<~RUBY, @cop)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert([], somestuff)
+          ^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_empty(somestuff)` over `assert([], somestuff)`.
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_asserting_empty_hash
+    assert_offense(<<~RUBY, @cop)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert({}, somestuff)
+          ^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_empty(somestuff)` over `assert({}, somestuff)`.
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_equal
+    assert_no_offenses(<<~RUBY, @cop)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_equal(['somestuff'], somestuff)
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_with_single_parameter
+    assert_no_offenses(<<~RUBY, @cop)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert(somestuff)
+        end
+      end
+    RUBY
+  end
+end
