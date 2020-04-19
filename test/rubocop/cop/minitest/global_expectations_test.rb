c34d96072f6457e455d187f7bf443744f2d4f575
Koichi ITO <koic.ito@gmail.com>
2020-04-20 00:33:02 +0900

    Suppress Ruby's warning
    This commit suppresses the following Ruby's wanring.
    
    ```console
    % bundle exec rake
    (snip)
    
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: method redefined; discarding old
    test_registers_offense_when_using_global_expectations_without_arguments
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: previous definition of
    test_registers_offense_when_using_global_expectations_without_arguments
    was here
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: method redefined; discarding old
    test_registers_offense_when_using_global_expectations_without_arguments
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: previous definition of
    test_registers_offense_when_using_global_expectations_without_arguments
    was here
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: method redefined; discarding old
    test_registers_offense_when_using_global_expectations_without_arguments
    /Users/koic/src/github.com/rubocop-hq/rubocop-minitest/test/rubocop/cop/minitest/global_expectations_test.rb:299:
    warning: previous definition of
    test_registers_offense_when_using_global_expectations_without_arguments
    was here
    Run options: --seed 20080
    ```

--- test/rubocop/cop/minitest/global_expectations_test.rb
+++ test/rubocop/cop/minitest/global_expectations_test.rb
@@ -295,20 +295,20 @@ class GlobalExpectationsTest < Minitest::Test
         end
       RUBY
     end
+  end
 
-    def test_registers_offense_when_using_global_expectations_without_arguments
-      assert_offense(<<~RUBY)
-        it 'does something' do
-          n.must_be_nil
-          ^ Use `_(n)` instead.
-        end
-      RUBY
+  def test_registers_offense_when_using_global_expectations_without_arguments
+    assert_offense(<<~RUBY)
+      it 'does something' do
+        n.must_be_nil
+        ^ Use `_(n)` instead.
+      end
+    RUBY
 
-      assert_correction(<<~RUBY)
-        it 'does something' do
-          _(n).must_be_nil
-        end
-      RUBY
-    end
+    assert_correction(<<~RUBY)
+      it 'does something' do
+        _(n).must_be_nil
+      end
+    RUBY
   end
 end
