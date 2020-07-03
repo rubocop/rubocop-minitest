30a46c12859f6285a1f3986cc9f6faaa85d46c86
fatkodima <fatkodima123@gmail.com>
2020-07-03 15:22:56 +0300

    Add new `Minitest/AssertionInLifecycleHook` cop

---
+++ test/rubocop/cop/minitest/assertion_in_lifecycle_hook_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AssertionInLifecycleHookTest < Minitest::Test
+  def test_registers_offense_when_using_bad_method
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def setup
+          assert_equal(foo, bar)
+          ^^^^^^^^^^^^^^^^^^^^^^ Do not use `assert_equal` in `setup` hook.
+        end
+      end
+    RUBY
+  end
+
+  def test_checks_only_test_classes
+    assert_no_offenses(<<~RUBY)
+      class Foo
+        def setup
+          assert_equal(foo, bar)
+        end
+      end
+    RUBY
+  end
+
+  def test_checks_only_hooks
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_equal(foo, bar)
+        end
+      end
+    RUBY
+  end
+end
