4c38d48715eed9c15f9c6089f29d982109405bdb
Koichi ITO <koic.ito@gmail.com>
2022-03-01 17:39:50 +0900

    Add new `Minitest/AssertPredicate` and `Minitest/RefutePredicate` cops
    This PR adds new `Minitest/AssertPredicate` and `Minitest/RefutePredicate` cops.
    
    ## `Minitest/AssertPredicate` cop
    
    This cop enforces the test to use `assert_predicate`
    instead of using `assert(obj.a_predicate_method?)`.
    
    ```ruby
    # bad
    assert(obj.one?)
    assert(obj.one?, 'message')
    
    # good
    assert_predicate(obj, :one?)
    assert_predicate(obj, :one?, 'message')
    ```
    
    ## `Minitest/RefutePredicate` cop
    
    This cop enforces the test to use `refute_predicate`
    instead of using `refute(obj.a_predicate_method?)`.
    
    ```ruby
    # bad
    refute(obj.one?)
    refute(obj.one?, 'message')
    
    # good
    refute_predicate(obj, :one?)
    refute_predicate(obj, :one?, 'message')
    ```

---
+++ test/rubocop/cop/minitest/refute_predicate_test.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RefutePredicateTest < Minitest::Test
+  def test_registers_offense_when_using_refute_with_predicate_method
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(obj.one?)
+          ^^^^^^^^^^^^^^^^ Prefer using `refute_predicate(obj, :one?)`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(obj, :one?)
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_refute_with_predicate_method_and_message
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(obj.one?, 'message')
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_predicate(obj, :one?, 'message')`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(obj, :one?, 'message')
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_refute_with_predicate_method_and_heredoc_message
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(obj.one?, <<~MESSAGE)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_predicate(obj, :one?, <<~MESSAGE)`.
+            message
+          MESSAGE
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(obj, :one?, <<~MESSAGE)
+            message
+          MESSAGE
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_refute_with_predicate_method_in_redundant_parentheses
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute((obj.one?))
+          ^^^^^^^^^^^^^^^^^^ Prefer using `refute_predicate(obj, :one?)`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(obj, :one?)
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_refute_with_receiver_omitted_predicate_method
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(one?)
+          ^^^^^^^^^^^^ Prefer using `refute_predicate(self, :one?)`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(self, :one?)
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_refute_predicate_method
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_predicate(obj, :one?)
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_registers_offense_when_using_refute_with_non_predicate_method
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(obj.do_something)
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_registers_offense_when_using_refute_with_local_variable
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          obj = create_obj
+          refute(obj)
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_refute_with_predicate_method_and_arguments
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute(obj.foo?(arg))
+        end
+      end
+    RUBY
+  end
+end
