64d57af0864a283b9d8386f0e860f77faf0008df
fatkodima <fatkodima123@gmail.com>
2022-07-25 22:26:14 +0300

    Add new `Minitest/AssertRaisesCompoundBody` cop

---
+++ changelog/new_assert_raises_compound_body_cop.md
@@ -0,0 +1,1 @@
+* [#109](https://github.com/rubocop/rubocop-minitest/issues/109): Add new `Minitest/AssertRaisesCompoundBody` cop. ([@fatkodima][])
---
+++ test/rubocop/cop/minitest/assert_raises_compound_body_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AssertRaisesCompoundBodyTest < Minitest::Test
+  def test_registers_offense_when_multi_statement_bodies
+    assert_offense(<<~RUBY)
+      assert_raises(MyError) do
+      ^^^^^^^^^^^^^^^^^^^^^^^^^ Reduce `assert_raises` block body to contain only the raising code.
+        foo
+        bar
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_single_statement_bodies
+    assert_no_offenses(<<~RUBY)
+      assert_raises(MyError) do
+        foo
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_begin_rescue
+    assert_no_offenses(<<~RUBY)
+      assert_raises(MyError) do
+        begin
+          foo
+          bar
+        rescue
+          baz
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_parenthesized_statement
+    assert_no_offenses(<<~RUBY)
+      assert_raises(MyError) do
+        (foo)
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_send_with_block_bodies
+    assert_no_offenses(<<~RUBY)
+      assert_raises(MyError) do
+        foo do
+          bar
+          baz
+        end
+      end
+    RUBY
+  end
+end
