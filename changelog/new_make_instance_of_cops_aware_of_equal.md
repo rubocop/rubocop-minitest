2d9e7ca8396c1cc0389f29aefcb4d7757d1cda89
Koichi ITO <koic.ito@gmail.com>
2023-03-27 22:35:35 +0900

    Make `Minitest/AssertInstanceOf` and `Minitest/RefuteInstanceOf` aware of `assert_equal` and `refute_equal`
    This PR makes `Minitest/AssertInstanceOf` and `Minitest/RefuteInstanceOf` aware of
    `assert_equal(Class, object.class)` and `refute_equal(Class, object.class)`.

---
+++ changelog/new_make_instance_of_cops_aware_of_equal.md
@@ -0,0 +1,1 @@
+* [#248](https://github.com/rubocop/rubocop-minitest/pull/248): Make `Minitest/AssertInstanceOf` and `Minitest/RefuteInstanceOf` aware of `assert_equal(Class, object.class)` and `refute_equal(Class, object.class)`. ([@koic][])
--- test/rubocop/cop/minitest/refute_instance_of_test.rb
+++ test/rubocop/cop/minitest/refute_instance_of_test.rb
@@ -66,12 +66,12 @@ class RefuteInstanceOfTest < Minitest::Test
     RUBY
   end
 
-  def test_registers_offense_when_using_refute_with_instance_of_in_redundant_parentheses
+  def test_registers_offense_when_using_refute_equal_with_class_arguments
     assert_offense(<<~RUBY)
       class FooTest < Minitest::Test
         def test_do_something
-          refute((object.instance_of?(SomeClass)))
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_instance_of(SomeClass, object)`.
+          refute_equal(SomeClass, obj.class)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_instance_of(SomeClass, obj)`.
         end
       end
     RUBY
@@ -79,13 +79,57 @@ class RefuteInstanceOfTest < Minitest::Test
     assert_correction(<<~RUBY)
       class FooTest < Minitest::Test
         def test_do_something
-          refute_instance_of((SomeClass, object))
+          refute_instance_of(SomeClass, obj)
         end
       end
     RUBY
   end
 
-  def test_refute_instance_of_method
+  def test_registers_offense_when_using_refute_equal_with_class_arguments_and_message
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_equal(SomeClass, obj.class, 'message')
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_instance_of(SomeClass, obj, 'message')`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_instance_of(SomeClass, obj, 'message')
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_refute_equal_with_class_arguments_and_heredoc_message
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_equal(SomeClass, obj.class, <<~MESSAGE
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `refute_instance_of(SomeClass, obj, <<~MESSAGE)`.
+            message
+          MESSAGE
+          )
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          refute_instance_of(SomeClass, obj, <<~MESSAGE
+            message
+          MESSAGE
+          )
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_refute_instance_of_method
     assert_no_offenses(<<~RUBY)
       class FooTest < Minitest::Test
         def test_do_something
