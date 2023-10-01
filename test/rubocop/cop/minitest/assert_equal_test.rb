060e7a07e98dd4836e3fa7eff0736c85996d7a57
Koichi ITO <koic.ito@gmail.com>
2023-10-01 19:10:33 +0900

    Make `Minitest/AssertEqual` aware of `assert_operator`
    This PR makes `Minitest/AssertEqual` aware of `assert_operator`.

--- test/rubocop/cop/minitest/assert_equal_test.rb
+++ test/rubocop/cop/minitest/assert_equal_test.rb
@@ -104,12 +104,12 @@ class AssertEqualTest < Minitest::Test
     RUBY
   end
 
-  def test_registers_offense_when_using_assert_with_equal_in_redundant_parentheses
+  def test_registers_offense_when_using_assert_operator_with_equal_symbol_argument
     assert_offense(<<~RUBY)
       class FooTest < Minitest::Test
         def test_do_something
-          assert(('rubocop-minitest' == actual))
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_equal('rubocop-minitest', actual)`.
+          assert_operator('rubocop-minitest', :==, actual)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_equal('rubocop-minitest', actual)`.
         end
       end
     RUBY
@@ -117,7 +117,26 @@ class AssertEqualTest < Minitest::Test
     assert_correction(<<~RUBY)
       class FooTest < Minitest::Test
         def test_do_something
-          assert_equal(('rubocop-minitest', actual))
+          assert_equal('rubocop-minitest', actual)
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_assert_operator_with_equal_symbol_and_message_argument
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_operator('rubocop-minitest', :==, actual, message)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_equal('rubocop-minitest', actual, message)`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_equal('rubocop-minitest', actual, message)
         end
       end
     RUBY
@@ -128,6 +147,27 @@ class AssertEqualTest < Minitest::Test
       class FooTest < Minitest::Test
         def test_do_something
           assert_equal('rubocop-minitest', actual)
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_assert_operator_with_negated_equal_symbol_argument
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_operator('rubocop-minitest', :!=, actual)
+        end
+      end
+    RUBY
+  end
+
+  # Redundant parentheses should be removed by `Style/RedundantParentheses` cop.
+  def test_does_not_register_offense_when_using_assert_with_equal_in_redundant_parentheses
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert(('rubocop-minitest' == actual))
         end
       end
     RUBY
