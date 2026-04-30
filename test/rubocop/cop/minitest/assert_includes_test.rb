1971c490ff536d2780aac02f7918298c08c5097d
Benjamin Quorning <benjamin@quorning.net>
2026-04-30 15:07:16 +0200

    Add assert_includes suggestion for assert_match with simple regexes
    Extend the AssertIncludes cop to suggest `assert_includes` instead of
    `assert_match` when the regex is a simple string literal without
    regex-specific features (anchors, quantifiers, character classes, etc.).

--- test/rubocop/cop/minitest/assert_includes_test.rb
+++ test/rubocop/cop/minitest/assert_includes_test.rb
@@ -146,4 +146,165 @@ class AssertIncludesTest < RuboCop::TestCase
       end
     RUBY
   end
+
+  def test_registers_offense_when_using_assert_match_with_simple_string_regexp
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo/, 'foobar')
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_includes('foobar', 'foo')`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_includes('foobar', 'foo')
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_assert_match_with_simple_string_regexp_and_message
+    assert_offense(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo/, 'foobar', 'message')
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_includes('foobar', 'foo', 'message')`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_includes('foobar', 'foo', 'message')
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_assert_match_with_escaped_url
+    assert_offense(<<~'RUBY')
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/http:\/\/example\.com/, response.body)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_includes(response.body, 'http://example.com')`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_includes(response.body, 'http://example.com')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_anchor
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/^foo/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_end_anchor
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo$/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_character_class
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/fo[ob]/, 'foobar') # codespell:ignore
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_quantifier
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo+/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_alternation
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo|bar/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_metacharacter
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo.bar/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_interpolation
+    assert_no_offenses(<<~'RUBY')
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo-#{bar}/, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_does_not_register_offense_when_using_assert_match_with_regex_options
+    assert_no_offenses(<<~RUBY)
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/foo/x, 'foobar')
+          assert_match(/foo/i, 'foobar')
+          assert_match(/foo/m, 'foobar')
+          assert_match(/foo/n, 'foobar')
+          assert_match(/foo/u, 'foobar')
+          assert_match(/foo/o, 'foobar')
+        end
+      end
+    RUBY
+  end
+
+  def test_registers_offense_when_using_assert_match_with_string_containing_both_quotes
+    assert_offense(<<~'RUBY')
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_match(/it's "working"/, response)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer using `assert_includes(response, "it's \"working\"")`.
+        end
+      end
+    RUBY
+
+    assert_correction(<<~'RUBY')
+      class FooTest < Minitest::Test
+        def test_do_something
+          assert_includes(response, "it's \"working\"")
+        end
+      end
+    RUBY
+  end
 end
