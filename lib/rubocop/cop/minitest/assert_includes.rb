6bd80ee925e931d72336f243b8d115e44618c615
Benjamin Quorning <benjamin@quorning.net>
2026-04-30 15:35:30 +0200

    Unpack the on_send method on assert_includes
    Before adding extra checks to assert_includes, it helps to unpack the
    on_send method first.

--- lib/rubocop/cop/minitest/assert_includes.rb
+++ lib/rubocop/cop/minitest/assert_includes.rb
@@ -19,6 +19,25 @@ module RuboCop
         extend MinitestCopRule
 
         define_rule :assert, target_method: %i[include? member?], preferred_method: :assert_includes
+
+        remove_method :on_send
+        def on_send(node)
+          handle_assert(node)
+        end
+
+        private
+
+        # Handle assert(collection.include?(object))
+        def handle_assert(node)
+          return unless node.method?(:assert)
+          return unless node.first_argument&.call_type?
+          return if node.first_argument.arguments.empty?
+          return unless %i[include? member?].include?(node.first_argument.method_name)
+
+          add_offense(node, message: offense_message(node.arguments)) do |corrector|
+            autocorrect(corrector, node, node.arguments)
+          end
+        end
       end
     end
   end
